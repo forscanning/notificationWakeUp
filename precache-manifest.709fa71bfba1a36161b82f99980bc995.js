55c4fd53b3341e5f71e662cecdc58e6047077bd1
Adam Charusta <charustaadam95@gmail.com>
2019-07-27 18:16:35 +0200

    Updates

---
+++ precache-manifest.709fa71bfba1a36161b82f99980bc995.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851570d17f5ede575eb4e16fabcbcb6f",
+    "url": "/notificationWakeUp/index.html"
+  },
+  {
+    "revision": "5c26224e1daf40335dbc",
+    "url": "/notificationWakeUp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9be674bfeaa20442c17a",
+    "url": "/notificationWakeUp/static/css/main.e98541af.chunk.css"
+  },
+  {
+    "revision": "5c26224e1daf40335dbc",
+    "url": "/notificationWakeUp/static/js/2.6ecbc426.chunk.js"
+  },
+  {
+    "revision": "9be674bfeaa20442c17a",
+    "url": "/notificationWakeUp/static/js/main.a2574f29.chunk.js"
+  },
+  {
+    "revision": "b63ddb945d1e57554a67",
+    "url": "/notificationWakeUp/static/js/runtime~main.4a98607e.js"
+  },
+  {
+    "revision": "255f9b9ad9257e937530d2f176756f5f",
+    "url": "/notificationWakeUp/static/media/instrukcja.255f9b9a.gif"
+  },
+  {
+    "revision": "6f8c9bacad1374a1fb5be00865bf01ef",
+    "url": "/notificationWakeUp/static/media/instrukcja_monkey.6f8c9bac.gif"
+  },
+  {
+    "revision": "8242367db5e2ac709ec175066b915df5",
+    "url": "/notificationWakeUp/static/media/test.8242367d.JPG"
+  }
+]);
